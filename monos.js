d664d54f5544ba7867e0216fe8c2d7fee9b5d4f2
Miguel Perez <migueledperezdelacruz452@gmail.com>
2023-01-15 21:41:34 -0400

    monitos

---
+++ monos.js
@@ -0,0 +1,12 @@
+const ASmile = false;
+const BSmile = false;
+const Alerta = "VERDADERO, ESTAMOS EN PROBLEMAS"
+
+if (ASmile == false && BSmile == false || ASmile == true && BSmile == true)
+{
+console.log(Alerta)
+}
+else
+{
+console.log("NO ESTAMOS EN PROBLEMAS");
+}
